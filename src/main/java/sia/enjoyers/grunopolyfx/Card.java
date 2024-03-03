7ccbc86dfcbe75a5dd5d24091d3b4140ca59b0a6
capyb29 <100852906+capyb29@users.noreply.github.com>
2024-03-03 18:10:41 +0100

    streets and stuff frfr

--- src/main/java/sia/enjoyers/grunopolyfx/Card.java
+++ src/main/java/sia/enjoyers/grunopolyfx/Card.java
@@ -5,29 +5,21 @@ import javafx.scene.layout.Pane;
 import java.util.ArrayList;
 
 public class Card {
-
-    public enum StreetColor {
-        BROWN,
-        LIGHTBLUE,
-        PINK,
-        ORANGE,
-        RED,
-        YELLOW,
-        GREEN,
-        BLUE,
-        None
-    }
-
     ArrayList<Player> playersOnCard;
     int price;
+    int rent;
     String name;
-    int houses;
+    Player owner;
 
-
-    Card(String name, int price, StreetColor color ) {
+    Card(String name, int price) {
         this.playersOnCard = new ArrayList<Player>();
         this.name = name;
         this.price = price;
-        this.houses = 0;
+        this.rent = 0;
+        this.owner = null;
+    }
+    public void buy(Player player) {
+
     }
 }
+
