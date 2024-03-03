b2bd16e7a19c93c6c1f710b127799a72838e99b6
10ki <vincentmauricewinkler@gmail.com>
2024-03-03 16:11:35 +0100

    New Player designs

--- src/main/java/sia/enjoyers/grunopolyfx/GrunopolyMain.java
+++ src/main/java/sia/enjoyers/grunopolyfx/GrunopolyMain.java
@@ -290,13 +290,11 @@ public class GrunopolyMain {
 
     public void initPlayers (int playerCount, String[] playerNames) {
         for (int i = 0; i < playerCount; i++) {
-            Color color = Color.color(Math.random(), Math.random(), Math.random());
-            Player player = new Player(playerNames[i], i + 1, color, 1000);
             Color hsb = Color.hsb((double) (((360 / playerCount) * (i + 1)) ), .7, .7);
 
-            Player player = new Player("Gru", i + 1, hsb, 1000);
-
+            Player player = new Player(playerNames[i], i + 1, hsb, 1000);
             player.setPosition(allPanes.getFirst(), 0);
+
             board.getChildren().add(player);
             players.add(player);
         }
