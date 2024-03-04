c6bc1131c2990ed69658f72244943d3400811f14
10ki <vincentmauricewinkler@gmail.com>
2024-03-04 16:55:18 +0100

    Fixed dice probability

--- src/main/java/sia/enjoyers/grunopolyfx/GrunopolyMain.java
+++ src/main/java/sia/enjoyers/grunopolyfx/GrunopolyMain.java
@@ -257,7 +257,9 @@ public class GrunopolyMain {
                 activePlayer++;
             }
 
-            this.diceNumber = (int) Math.max(2, 1 + (Math.random() * 12));
+            int dice1 = (int) (1 + (Math.random() * 6));
+            int dice2 = (int) (1 + (Math.random() * 6));
+            this.diceNumber = dice1 + dice2;
             step(this.diceNumber, players.get(activePlayer));
         });
 
